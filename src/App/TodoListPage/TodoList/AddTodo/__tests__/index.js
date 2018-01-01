f46e693730c0279890ab9a15b113034795ffab18
Danilo Pimentel <danilo.pimentel97@gmail.com>
2018-01-01 21:17:49 -0200

    TST: Adding tests to the AddTodo component.

--- src/App/TodoListPage/TodoList/AddTodo/__tests__/index.js
+++ src/App/TodoListPage/TodoList/AddTodo/__tests__/index.js
@@ -1,9 +1,71 @@
 import React from 'react';
-import basicComponentTests, {
-  createStoreMock,
-} from 'tests/basicComponentTests';
+import { Input, Button } from 'semantic-ui-react';
+import { INITIAL_STATE } from 'common/ducks';
+import configureMockStore from 'redux-mock-store';
 import AddTodo from '../';
 
+const mockStore = configureMockStore();
+
 describe('AddTodo', () => {
-  basicComponentTests(<AddTodo store={createStoreMock()} />);
+  let renderedAddTodo;
+
+  const addTodo = (options = { render: mount }) => {
+    if (!renderedAddTodo) {
+      renderedAddTodo = options
+        .render(<AddTodo store={mockStore(INITIAL_STATE)} />);
+    }
+
+    return renderedAddTodo;
+  };
+
+  beforeEach(() => {
+    renderedAddTodo = undefined;
+  });
+
+  it('should shallow render the component', () => {
+    expect(addTodo({ render: shallow })).toMatchSnapshot();
+  });
+
+  it('always renders a div that contains everything else', () => {
+    const divs = addTodo().find('div');
+    expect(divs.length).toBeGreaterThan(0);
+  });
+
+  it('always renders a "Input"', () => {
+    expect(addTodo().find(Input).length).toBe(1);
+  });
+
+  it('always renders a "Button"', () => {
+    expect(addTodo().find(Button).length).toBe(1);
+  });
+
+  describe('when the user is typing', () => {
+    const input = addTodo().find(Input).first();
+    const value = 'User typing';
+    const eventMock = { event: { target: { value } } };
+
+    input.simulate('change', eventMock);
+    addTodo().update();
+
+    it('should update the input value', () => {
+      expect(input.props().value).toBe(value);
+    });
+
+    it('should update the state value', () => {
+      throw new Error('To be implemented');
+    });
+  });
+
+  describe('when the user hits the "Enter" key', () => {
+    it('should dispatch a new ADD_TODO action', () => {
+      throw new Error('To be implemented');
+    });
+
+    it('clear the state value', () => {
+      throw new Error('To be implemented');
+    });
+  });
+
+  // // A Input is always rendered. It receives value, onChange and onKeyPress
+  // // A Button is always rendered. It receives onClick
 });
